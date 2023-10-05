0e61fea091307088a3b60ef5b4e72a777bf7c90c
pass42 <dnop030@gmail.com>
2023-10-06 05:24:41 +0700

    -Add Method to accept connection on ServerHandle
    -be able to Recv Package from accepted connection
    
    Code not clean

--- include/ServHandle.hpp
+++ include/ServHandle.hpp
@@ -4,10 +4,29 @@
 #include <iostream>
 #include <string>
 
-#include <vector>
+#include <sys/socket.h>
+// #include <vector>
+#include <map>
+
+// test epool
+#include "sys/epoll.h"
+
+// socket
+#include "netinet/in.h"
+#include "sys/socket.h"
+
+// errno
+#include "errno.h"
+
+// for generate response
+#include <sstream>
 
 #include "ConfigFileHandle.hpp"
-#include "Server.hpp"
+
+// #include "stdio.h"
+
+#define MAXEVENTS 128
+#define BUFFPACK 2048
 
 class ServHandle
 {
@@ -23,9 +42,43 @@ class ServHandle
 		ServHandle(ServHandle const & r);
 		ServHandle &operator=(ServHandle const & r);
 
+		void	createServ(std::string const & config);
+		// int		findVec(std::vector<int> const & vec, int val);
+		void	showMapFd(void);
+
+		void	sockServRd(int const & servFd);
+		void	sockServWr(void);
+		void	sockCliRd(int const & cliFd);
+		void	sockCliWr(void);
+
+		std::string	generateHttpResponse(int statusCode, std::string const & statusMessage, std::string const & content);
+
 		ConfigFileHandle		_configServ;
-		std::vector<Server *>	_serv;
-		Server 					*_tmpServer;
+		// std::vector<Server *>	_serv;
+		// Server 					*_tmpServer;
+
+		// using only when create socket server
+		// std::vector<int>		_servFd;
+		std::map<int, char>		_mapFd;
+
+		struct sockaddr_in		_address;
+		int						_fd;
+
+		int						_epoll_fd;
+		struct epoll_event		_event;
+		struct epoll_event		*_event_ret;
+
+		// std::vector<int>		_cliFd;
+		struct sockaddr			_inAddr;
+		socklen_t				_inLen;
+		int						_infd;
+		char					_buffRd[BUFFPACK];
+		std::string				_bufferPack;
+		int						_valRd;
+		std::string				_bufferSend;
+
+		int						_tmpInt;
+		std::string				_tmpStdStr;
 };
 
 #endif
--- src/ServHandle.cpp
+++ src/ServHandle.cpp
@@ -1,11 +1,16 @@
 #include "ServHandle.hpp"
+#include <cstdlib>
+#include <fcntl.h>
+#include <sys/epoll.h>
+#include <sys/socket.h>
+#include <unistd.h>
 
 ServHandle::ServHandle(void) {}
 
 ServHandle::~ServHandle(void) {
-	for (int i=0; i<this->_serv.size(); i++) {
-		delete this->_serv[i];
-	}
+	// for (int i=0; i<this->_serv.size(); i++) {
+	// 	delete this->_serv[i];
+	// }
 }
 
 void	ServHandle::servCreate(char const * configFile) {
@@ -23,14 +28,393 @@ void	ServHandle::servCreate(char const * configFile) {
 	// std::cout << MAG << "[INFO]output \"listen \" 0 " << tmpStr << reset << std::endl;
 	// // std::cout << MAG << "[INFO]output length " << tmpStr.length() << reset << std::endl;
 
+	this->showMapFd();
+
 	std::cout << MAG << "amout of serv config " << this->_configServ.getAmountServConfig() << reset << std::endl;
 	for (int i=0; i<this->_configServ.getAmountServConfig(); i++) {
-		this->_tmpServer = new Server();
-		this->_tmpServer->setServSockAddr(this->_configServ.getServConfigVal(i, "listen"));
-		this->_serv.push_back(this->_tmpServer);
+		this->createServ(this->_configServ.getServConfigVal(i, "listen"));
+		// std::cout << MAG << "[INFO]fd of server " << this->_servFd[i] << reset << std::endl;
+	}
+
+	// epolll create
+	// if flag = 0 => it is similar to epoll_create
+	this->_epoll_fd = epoll_create1(0);
+	if (this->_epoll_fd == -1) {
+		std::cerr << RED << "Failed to create epoll file descriptor" << reset << std::endl;
+		exit (EXIT_FAILURE);
+	}
+
+	// // Add Fd of server into epoll instance
+	// for (int i=0; i<this->_configServ.getAmountServConfig(); i++) {
+	// 	this->_event.data.fd = this->_servFd[i];
+	// 	// event.events = EPOLLIN | EPOLLET;
+	// 	this->_event.events = EPOLLIN | EPOLLOUT;
+	// 	// event.events = EPOLLIN | EPOLLOUT | EPOLLET;
+
+	// 	this->_tmpInt = epoll_ctl (this->_epoll_fd, EPOLL_CTL_ADD, this->_servFd[i], &(this->_event));
+	// 	if (this->_tmpInt < 0) {
+	// 		std::cerr << "Failed to add server file descriptor in instance epoll of server " << i << std::endl;
+	// 	}
+	// }
+	// Add Fd of server into epoll instance
+	for (std::map<int, char>::iterator it = this->_mapFd.begin(); it != this->_mapFd.end(); it++) {
+		this->_event.data.fd = it->first;
+		// event.events = EPOLLIN | EPOLLET;
+		this->_event.events = EPOLLIN | EPOLLOUT;
+		// event.events = EPOLLIN | EPOLLOUT | EPOLLET;
+
+		this->_tmpInt = epoll_ctl (this->_epoll_fd, EPOLL_CTL_ADD, it->first, &(this->_event));
+		if (this->_tmpInt < 0) {
+			std::cerr << RED << "Failed to add server file descriptor in instance epoll of server " << it->first << reset << std::endl;
+		}
+	}
+
+	// Show all Server Fd
+	// std::cout << MAG << "Show serv Fd" << reset << std::endl;
+	// for (int i=0; i<this->_configServ.getAmountServConfig(); i++) {
+	// 	std::cout << this->_servFd[i] << std::endl;
+	// }
+	this->showMapFd();
+
+	// Need to fix -> using new method
+	this->_event_ret = (struct epoll_event *)calloc(MAXEVENTS, sizeof(struct epoll_event));
+}
+
+void	ServHandle::servStart(void) {
+
+	while (1) {
+		int	numEvent, i;
+
+		numEvent = epoll_wait (this->_epoll_fd, this->_event_ret, MAXEVENTS, -1);
+		std::cout << std::endl << MAG << "[INFO]found " << numEvent << " event(s)" << reset << std::endl;
+
+		for (i=0; i<numEvent; i++) {
+			if ((this->_event_ret[i].events & EPOLLERR) || (this->_event_ret[i].events & EPOLLHUP)) {
+				/* An error has occured on this fd, or the socket is not
+					ready for reading (why were we notified then?) */
+				std::cerr << RED << "[ERROR] epoll error" << reset << std::endl;
+				std::cerr << RED << "[ERROR] error @ fd " << this->_event_ret[i].data.fd << reset << std::endl;
+				close(this->_event_ret[i].data.fd);
+			}
+			else {
+				if (this->_event_ret[i].events & EPOLLIN) {
+					std::cout << MAG << "[INFO] Found Rd on " << this->_event_ret[i].data.fd << reset << std::endl;
+					std::map<int, char>::iterator	it;
+
+					it = this->_mapFd.find(this->_event_ret[i].data.fd);
+					if (it != this->_mapFd.end()) {
+						if (it->second == 's') {
+							// go to server Rd Accept
+							this->sockServRd(it->first);
+						}
+						else {
+							// go to Rd Request
+							this->sockCliRd(it->first);
+						}
+					}
+					else {
+						std::cout << RED << "[ERROR] fake fd num " << this->_event_ret[i].data.fd << reset << std::endl;
+					}
+				}
+				else if (this->_event_ret[i].events & EPOLLOUT) {
+					std::cout << MAG << "[INFO] Found Wr on " << this->_event_ret[i].data.fd << reset << std::endl;
+					std::map<int, char>::iterator	it;
+
+					it = this->_mapFd.find(this->_event_ret[i].data.fd);
+					if (it != this->_mapFd.end()) {
+						if (it->second == 's') {
+							// go to server Wr What???
+						}
+						else {
+							// go to Wr Response
+						}
+					}
+					else {
+						std::cout << RED << "[ERROR] fake fd num " << this->_event_ret[i].data.fd << reset << std::endl;
+					}
+				}
+				else {
+					std::cout << RED << "[WARNING] found flag not rd and wr on " << this->_event_ret[i].data.fd << " with flag " << this->_event_ret[i].events << std::endl;
+				}
+			}
+		}
+	}
+
+	// while (1) {
+	// 	int	n, i;
+
+	// 	// timeout = -1 is wait infinity, timeout = 0 is return immediately
+	// 	n = epoll_wait(this->_epoll_fd, this->_event_ret, MAXEVENTS, -1);
+	// 	std::cout << std::endl << MAG << "[INFO] " << n << " events" << reset << std::endl;
+
+	// 	for (i=0; i<n; i++) {
+	// 		if ((this->_event_ret[i].events & EPOLLERR) || (this->_event_ret[i].events & EPOLLHUP)) {
+	// 			/* An error has occured on this fd, or the socket is not
+	// 				ready for reading (why were we notified then?) */
+	// 			std::cerr << RED << "[ERROR] epoll error" << reset << std::endl;
+	// 			std::cerr << RED << "[ERROR] error @ fd " << this->_event_ret[i].data.fd << reset << std::endl;
+	// 			close(this->_event_ret[i].data.fd);
+	// 		}
+	// 		else {
+	// 			this->_tmpInt = this->findVec(this->_servFd, this->_event_ret[i].data.fd);
+	// 			// std::cout << MAG << "[INFO]Server Socket Fd " << this->_servFd[this->_tmpInt] << " get access" << reset << std::endl;
+
+	// 			if (this->_tmpInt >= 0) {
+
+	// 				std::cout << CYN << "[INFO]show FLAG " << this->_event_ret[i].events << reset << std::endl;
+
+	// 				// this->_inLen = sizeof(this->_inAddr);
+	// 				// this->_infd = accept(this->_servFd[this->_tmpInt], &(this->_inAddr), &(this->_inLen));
+	// 				this->_infd = accept(this->_servFd[this->_tmpInt], NULL, NULL);
+	// 				if (this->_infd == -1) {
+	// 					if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+	// 						/* We have processed all incoming
+	// 							connections. */
+	// 						// need to be handle ???????????????????????????
+	// 						break;
+	// 					}
+	// 					else {
+	// 						perror ("accept");
+	// 						break;
+	// 					}
+	// 				}
+
+	// 				// Make the accepted socket non-blocking
+	// 				int flags = fcntl(this->_infd, F_GETFL, 0);
+	// 				if (flags == -1) {
+	// 					perror("fcntl get");
+	// 					close(this->_infd);
+	// 				}
+	// 				if (fcntl(this->_infd, F_SETFL, flags | O_NONBLOCK) == -1) {
+	// 					perror("fcntl set");
+	// 					close(this->_infd);
+	// 				}
+
+	// 				// register new socket to epoll
+	// 				this->_event.data.fd = this->_infd;
+	// 				this->_event.events = EPOLLIN | EPOLLOUT | EPOLLET;
+	// 				this->_tmpInt = epoll_ctl(this->_epoll_fd, EPOLL_CTL_ADD, this->_infd, &(this->_event));
+	// 				if (this->_tmpInt == -1) {
+	// 					perror("epoll_ctl accept");
+	// 					abort();
+	// 				}
+
+	// 				// Add client Fd into vector
+	// 				this->_cliFd.insert(this->_cliFd.begin(), this->_infd);
+
+
+	// 				// this->_bufferPack.clear();
+	// 				// do {
+	// 				// 	// this->_valRd = recv(this->_infd, this->_buffRd, BUFFPACK, 0);
+	// 				// 	this->_valRd = recv(this->_infd, this->_buffRd, sizeof(this->_buffRd), 0);
+	// 				// 	if (this->_valRd > 0) {
+	// 				// 		std::cout << "Recv " << this->_valRd << std::endl;
+	// 				// 		this->_tmpStdStr = this->_buffRd;
+	// 				// 		this->_bufferPack += this->_tmpStdStr;
+	// 				// 		// this->_bufferPack.append(this->_buffRd, this->_valRd);
+	// 				// 	}
+	// 				// }
+	// 				// while (this->_valRd > 0);
+	// 				// std::cout << YEL << "valRd " << this->_valRd << reset << std::endl;
+
+	// 				// // if (this->_valRd == -1) {
+	// 				// // 	// Handle error if needed
+	// 				// // 	perror("recv");
+	// 				// // }
+
+	// 				// std::cout << CYN << "Data in Package" << reset << std::endl;
+	// 				// std::cout << CYN << this->_bufferPack << reset << std::endl;
+	// 				// std::cout << YEL << this->_tmpStdStr << reset << std::endl;
+
+	// 				// this->_bufferSend = generateHttpResponse(200, "OK", "Hello World");
+	// 				// // send(int fd, const void *buf, size_t n, int flags)
+	// 				// send(this->_infd, this->_bufferSend.c_str(), this->_bufferSend.size(), 0);
+
+	// 			}
+	// 		}
+	// 	}
+	// }
+}
+
+void	ServHandle::servStop(void) {}
+
+void	ServHandle::createServ(std::string const & config) {
+	int	opt = 1;
+	// std::vector<int>::iterator	it = this->_servFd.begin();
+	std::map<int, char>::iterator	it = this->_mapFd.begin();
+
+	// std::cout << MAG << "config " << config << reset << std::endl;
+
+	this->_address.sin_family = AF_INET;
+	this->_address.sin_addr.s_addr = INADDR_ANY;
+	this->_address.sin_port = htons(atoi(config.c_str()));
+
+	if ((this->_fd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0)) < 0) {
+	// if ((this->_fd = socket(AF_INET, SOCK_NONBLOCK, 0)) < 0) {
+		perror("socket failed");
+		exit (EXIT_FAILURE);
+	}
+	// std::cout << YEL << "socket fd "<< this->_fd << reset << std::endl;
+
+	// Forcefully attaching socket to the port
+	if (setsockopt(this->_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+		perror("setsockopt");
+		exit (EXIT_FAILURE);
+	}
+	// std::cout << YEL << "socket opt" << reset << std::endl;
+
+	// Forcefully attaching socket to the port
+	if (bind(this->_fd, (struct sockaddr *)&(this->_address), sizeof(struct sockaddr)) < 0) {
+	// if (bind(this->_fd, static_cast<struct sockaddr *>(&(this->_address)), sizeof(struct sockaddr)) < 0) {
+		perror("bind failed");
+		exit (EXIT_FAILURE);
+	}
+	// std::cout << YEL << "bind" << reset << std::endl;
+
+	// changing socket from active to passive
+	if (listen(this->_fd, 1024) < 0) {
+		perror("listen");
+		exit (EXIT_FAILURE);
+	}
+
+	// // Add Fd into vector
+	// this->_servFd.insert(it, this->_fd);
+
+	// Add Fd into map
+	it = this->_mapFd.find(this->_fd);
+	if (it == this->_mapFd.end()) {
+		this->_mapFd.insert(std::pair<int, char>(this->_fd, 's'));
+	}
+	else {
+		std::cerr << std::endl << RED << "[ERROR] Found duplicate fd" << reset << std::endl;
 	}
 }
 
-void	ServHandle::servStart(void) {}
+// int		ServHandle::findVec(std::vector<int> const & vec, int val) {
+// 	int	i = 0;
 
-void	ServHandle::servStop(void) {}
+// 	while ((vec[i] != val) && (i < vec.size()))
+// 		i++;
+// 	if (i < vec.size())
+// 		return (i);
+// 	else
+// 		return (-1);
+// }
+
+void	ServHandle::showMapFd(void) {
+	std::map<int, char>::iterator	it = this->_mapFd.begin();
+
+	std::cout << std::endl << "Show Map Fd" << std::endl;
+	// std::cout << "fd s/c" << std::endl;
+	while (it != this->_mapFd.end()) {
+		std::cout << it->first << " " << it->second << std::endl;
+		it++;
+	}
+	std::cout << "-----------" << std::endl;
+	std::cout << std::endl;
+}
+
+void	ServHandle::sockServRd(int const & servFd) {
+	std::map<int, char>::iterator	it;
+	// this->_inLen = sizeof(this->_inAddr);
+	// this->_infd = accept(this->_servFd[this->_tmpInt], &(this->_inAddr), &(this->_inLen));
+	this->_infd = accept(servFd, NULL, NULL);
+	if (this->_infd == -1) {
+		if ((errno == EAGAIN) || (errno == EWOULDBLOCK)) {
+			/* We have processed all incoming
+				connections. */
+			// need to be handle ???????????????????????????
+			return ;
+		}
+		else {
+			perror ("accept");
+			return ;
+		}
+	}
+
+	// Make the accepted socket non-blocking
+	// int flags = fcntl(this->_infd, F_GETFL, 0);
+	// if (flags == -1) {
+	// 	perror("fcntl get");
+	// 	close(this->_infd);
+	// }
+	// if (fcntl(this->_infd, F_SETFL, flags | O_NONBLOCK) == -1) {
+	if (fcntl(this->_infd, F_SETFL, O_NONBLOCK) == -1) {
+		perror("fcntl set");
+		close(this->_infd);
+	}
+
+	// register new socket to epoll
+	this->_event.data.fd = this->_infd;
+	this->_event.events = EPOLLIN | EPOLLOUT | EPOLLET;
+	this->_tmpInt = epoll_ctl(this->_epoll_fd, EPOLL_CTL_ADD, this->_infd, &(this->_event));
+	if (this->_tmpInt == -1) {
+		perror("epoll_ctl accept");
+		abort();
+	}
+
+	// Add client Fd into map
+	// this->_cliFd.insert(this->_cliFd.begin(), this->_infd);
+	it = this->_mapFd.find(this->_infd);
+	if (it == this->_mapFd.end()) {
+		this->_mapFd.insert(std::pair<int, char>(this->_infd, 'c'));
+	}
+	else {
+		std::cerr << std::endl << RED << "[ERROR] Found duplicate fd when accept" << reset << std::endl;
+	}
+	// // Add client Fd into vector
+	// this->_cliFd.insert(this->_cliFd.begin(), this->_infd);
+
+	// for testing accept function
+	this->showMapFd();
+}
+
+void	ServHandle::sockServWr(void) {}
+
+void	ServHandle::sockCliRd(int const & cliFd) {
+	this->_bufferPack.clear();
+	do {
+		// this->_valRd = recv(this->_infd, this->_buffRd, sizeof(this->_buffRd), 0);
+		// this->_valRd = read(this->_infd, this->_buffRd, sizeof(this->_buffRd));
+		this->_valRd = recv(this->_infd, this->_buffRd, BUFFPACK, 0);
+		std::cout << "Recv " << this->_valRd << std::endl;
+		if (this->_valRd > 0) {
+			// printf("\n\n%s\n\n", this->_buffRd);
+			// std::cout << this->_buffRd << std::endl;
+			this->_tmpStdStr = this->_buffRd;
+			this->_bufferPack += this->_tmpStdStr;
+			// this->_bufferPack.append(this->_buffRd, this->_valRd);
+		}
+	}
+	while (this->_valRd > 0);
+	std::cout << YEL << "valRd " << this->_valRd << reset << std::endl;
+
+	// if (this->_valRd == -1) {
+	// 	// Handle error if needed
+	// 	perror("recv");
+	// }
+
+	std::cout << CYN << "Data in Package" << reset << std::endl;
+	std::cout << CYN << this->_bufferPack << reset << std::endl;
+	std::cout << YEL << this->_tmpStdStr << reset << std::endl;
+}
+
+void	ServHandle::sockCliWr(void) {}
+
+std::string	ServHandle::generateHttpResponse(int statusCode, std::string const & statusMessage, std::string const & content) {
+	// Create an output stream to build the response
+	std::ostringstream responseStream;
+
+	// HTTP status line
+	responseStream << "HTTP/1.1 " << statusCode << " " << statusMessage << "\r\n";
+
+	// Headers
+	responseStream << "Content-Length: " << content.length() << "\r\n";
+	responseStream << "Content-Type: text/plain\r\n"; // Adjust content type as needed
+	responseStream << "\r\n"; // End of headers
+
+	// Response content
+	responseStream << content;
+
+	return responseStream.str();
+}
