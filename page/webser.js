77805860e4286a6944c3fe6c3544ab018bb0de11
LMikadaL <panupong.mikada@gmail.com>
2023-10-16 21:47:07 +0700

    update input port

--- page/webser.js
+++ page/webser.js
@@ -1,4 +1,5 @@
 const method = document.getElementById("method");
+const port = document.getElementById("port");
 const url = document.getElementById("url");
 const submit = document.getElementById("submit");
 const output = document.getElementById('output');
@@ -6,12 +7,13 @@ const output = document.getElementById('output');
 submit.addEventListener("click", (e) => {
 	e.preventDefault(); // not refresh page
 
-	const v_method = method.value;
-	const v_url = url.value;
+	const v_method = method.value ? method.value : "GET";
+	const v_port = port.value ? port.value : "443";
+	const v_url = url.value ? url.value : "https://127.0.0.1";
 
 	axios.request({
 		method: v_method,
-		url: v_url
+		url: v_url + ":" + v_port,
 	}).then(function (response) {
 		// console.log(response); // show all response
 		// let txt = JSON.stringify(response.data); // convert JSON to string
@@ -21,3 +23,11 @@ submit.addEventListener("click", (e) => {
 		console.log(error);
 	});
 });
+
+const onlyNumberKey = (evt) => {            
+	let ASCIICode = (evt.which) ? evt.which : evt.keyCode
+
+	if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
+		return false;
+	return true;
+}
