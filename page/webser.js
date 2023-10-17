a9eeedaa5cc159572cc761e490784c54bfc48778
LMikadaL <panupong.mikada@gmail.com>
2023-10-17 22:18:32 +0700

    edit file index

--- page/webser.js
+++ page/webser.js
@@ -1,33 +1,20 @@
-const method = document.getElementById("method");
-const port = document.getElementById("port");
-const url = document.getElementById("url");
-const submit = document.getElementById("submit");
-const output = document.getElementById('output');
+const uploadfile = document.getElementById("uploadfile");
+const delfile = document.getElementById("delfile");
 
-submit.addEventListener("click", (e) => {
+uploadfile.addEventListener("click", (e) => {
 	e.preventDefault(); // not refresh page
 
-	const v_method = method.value ? method.value : "GET";
-	const v_port = port.value ? port.value : "443";
-	const v_url = url.value ? url.value : "https://127.0.0.1";
-
-	axios.request({
-		method: v_method,
-		url: v_url + ":" + v_port,
-	}).then(function (response) {
-		// console.log(response); // show all response
-		// let txt = JSON.stringify(response.data); // convert JSON to string
-		// output.innerHTML = txt; // open when response is JSON
-		output.innerHTML = response.data; // open when response is text
-	}).catch(function (error) {
-		console.log(error);
-	});
+	axios.post('http://127.0.0.1', {
+			photo: document.querySelector('#file').files
+		}, {
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		}); // change url
 });
 
-const onlyNumberKey = (evt) => {            
-	let ASCIICode = (evt.which) ? evt.which : evt.keyCode
+delfile.addEventListener("click", (e) => {
+	e.preventDefault(); // not refresh page
 
-	if (ASCIICode > 31 && (ASCIICode < 48 || ASCIICode > 57))
-		return false;
-	return true;
-}
+	axios.delete('http://127.0.0.1'); // change url
+});
