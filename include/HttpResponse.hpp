913570eafe16f4ec70aeeb846fa12128cda3cfda
LMikadaL <panupong.mikada@gmail.com>
2023-11-05 20:02:11 +0700

    update response

--- include/HttpResponse.hpp
+++ include/HttpResponse.hpp
@@ -2,29 +2,47 @@
 #define HTTPREPONSE_HPP
 
 #include "Http.hpp"
+#include "ConfigFileHandle.hpp"
 #include <fstream>
 #include <sstream>
 
 class HttpResponse : public Http
 {
 	private:
-		int			_statusCode;
-		std::string _statusMessage;
-		std::string _fileResponse;
-		std::string	checkFile();
-		std::map<std::string, std::string> _header;
+		int					_statusCode;
+		std::string 		_statusMessage;
+		std::string 		_fileResponse;
+		std::string 		_serverName;
+		std::string 		_path;
+		std::string 		_port;
+		std::string			_method;
+		ConfigFileHandle	*_config;
+		std::map<std::string, std::string>	_header;
+		std::map<int, std::string>			_status;
+		std::map<int, std::string>			_fileError;
+		// read from ConfigFileHandle
+		int					_config_ser;
+		std::string			_config_location;
+		std::vector<std::string>	_config_condition;
+		// func check before send response
+		int					_checkPort();
+		int					_checkPath();
+		void				_setHeader(std::string const &key, std::string const &value);
+		void				_setConfigCondition();
+		std::string			_checkFile();
+		std::string			_setResponseStream();
 
-	// protected:
 	public:
 		HttpResponse();
 		~HttpResponse();
 		HttpResponse(HttpResponse const &rhs);
 		HttpResponse	&operator=(HttpResponse const &rhs);
 
-		void			setStatusCode(int statusCode);
-		void			setStatusMessage(std::string const &statusMessage);
-		void			setFileResponse(std::string const &fileResponse);
-		void			setHeader(std::string const &key, std::string const &value);
+		void			setServername(std::string const &servername);
+		void			setPath(std::string const &path);
+		void			setPort(std::string const &port);
+		void			setMethod(std::string const &method);
+		void			setConfig(ConfigFileHandle *config);
 		std::string		returnResponse();
 };
 
--- src/HttpResponse.cpp
+++ src/HttpResponse.cpp
@@ -2,6 +2,22 @@
 
 HttpResponse::HttpResponse()
 {
+	this->_config_ser = -1;
+	this->_status[100] = "Continue";
+	this->_status[101] = "Switching Protocols";
+	this->_status[200] = "OK";
+	this->_status[201] = "Created";
+	this->_status[202] = "Accepted";
+	this->_status[400] = "Bad Request";
+	this->_status[404] = "Not Found";
+	this->_status[405] = "Method Not Allowed";
+	this->_status[411] = "Length Required";
+	this->_status[413] = "Request Entity Too Large";
+	this->_status[500] = "Internal Server Error";
+	this->_status[501] = "Not Implemented";
+	this->_status[505] = "HTTP Version not supported";
+	this->_fileError[404] = "page/404.html";
+	this->_fileError[500] = "page/500.html";
 }
 
 HttpResponse::~HttpResponse()
@@ -21,22 +37,27 @@ HttpResponse	&HttpResponse::operator=(HttpResponse const &rhs)
 	return *this;
 }
 
-void	HttpResponse::setStatusCode(int statusCode)
+void	HttpResponse::setServername(std::string const &servername)
 {
-	this->_statusCode = statusCode;
+	this->_serverName = servername;
 }
 
-void	HttpResponse::setStatusMessage(std::string const &statusMessage)
+void	HttpResponse::setPath(std::string const &path)
 {
-	this->_statusMessage = statusMessage;
+	this->_path = path;
 }
 
-void	HttpResponse::setFileResponse(std::string const &fileResponse)
+void	HttpResponse::setPort(std::string const &port)
 {
-	this->_fileResponse = fileResponse;
+	this->_port = port;
 }
 
-std::string	HttpResponse::checkFile()
+void	HttpResponse::setMethod(std::string const &method)
+{
+	this->_method = method;
+}
+
+std::string	HttpResponse::_checkFile()
 {
 	std::ifstream ifs(this->_fileResponse);
 
@@ -44,35 +65,90 @@ std::string	HttpResponse::checkFile()
 		std::string content((std::istreambuf_iterator<char>(ifs)), std::istreambuf_iterator<char>());
 		return content;
 	} else {
-		this->_statusCode = 404;
-		this->_statusMessage = "Not Found";
-		std::string content("<h1>404 not found</h1>");
+		std::string content("<h1>404 not found file</h1>");
 		return content;
 	}
 }
 
-void	HttpResponse::setHeader(std::string const &key, std::string const &value)
+void	HttpResponse::setConfig(ConfigFileHandle *config)
+{
+	this->_config = config;
+}
+
+void	HttpResponse::_setHeader(std::string const &key, std::string const &value)
 {
 	this->_header[key] = value;
 }
 
-std::string	HttpResponse::returnResponse()
+int	HttpResponse::_checkPort()
+{
+	unsigned int amount_ser = this->_config->getAmountServConfig();
+	for (int i = 0; i < amount_ser; i++) {
+		if (this->_port.compare(this->_config->getServConfigVal(i, "listen")) == 0) {
+			this->_config_ser = i;
+			break;
+		}
+	}
+	return (this->_config_ser);
+}
+
+int	HttpResponse::_checkPath()
+{
+	this->_config_location = (this->_config_ser > -1) ? this->_config->getServConfigVal(this->_config_ser, "location " + this->_path) : "";
+
+	return this->_config_location.length();
+}
+
+void HttpResponse::_setConfigCondition()
+{
+	std::string condition = this->_config_location;
+	std::string delimiter = ";";
+	size_t pos = 0;
+	std::string token;
+
+	while ((pos = condition.find(delimiter)) != std::string::npos) {
+		token = condition.substr(0, pos);
+		this->_config_condition.push_back(token);
+		condition.erase(0, pos + delimiter.length());
+	}
+	this->_config_condition.push_back(condition);
+}
+
+std::string	HttpResponse::_setResponseStream()
 {
 	std::ostringstream resStream;
-	std::ifstream ifs(this->_fileResponse);
-	std::string contentRes = this->checkFile();
+	std::string contentRes = this->_checkFile();
 	std::map<std::string, std::string>::iterator it;
 
-	setHeader("Content-Length:", std::to_string(contentRes.length()));
-	setHeader("Content-Type:", "text/html");
-	setHeader("Accept-Language:", "TH");
+	this->_setHeader("Content-Length:", std::to_string(contentRes.length()));
+	this->_setHeader("Content-Type:", "text/html");
+	this->_setHeader("Accept-Language:", "TH");
 
-	resStream << "HTTP/1.1 " << this->_statusCode << " " << this->_statusMessage << "\r\n";
+	resStream << "HTTP/1.1 " << this->_statusCode << " " << this->_status[this->_statusCode] << "\r\n";
 	for (it = this->_header.begin(); it != this->_header.end(); it++) {
 		resStream << it->first << " " << it->second << "\r\n";
 	}
+	// resStream << "test-path: " << this->_path << "\r\n";
+	// resStream << "test-condition: " << this->_config_location << "\r\n";
 	resStream << "\r\n";
 	resStream << contentRes;
 
 	return resStream.str();
+}
+
+std::string	HttpResponse::returnResponse()
+{
+	try {
+		if (this->_checkPort() > -1 && this->_checkPath()) {
+			this->_setConfigCondition();
+			this->_statusCode = 200;
+			this->_fileResponse = "page/index.html";
+		} else
+			throw (404);
+	} catch (int status) {
+		this->_statusCode = status;
+		this->_fileResponse = this->_fileError[status];
+	}
+
+	return (this->_setResponseStream());
 }
